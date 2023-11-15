d7e51c4e049facfd66ecae3cbba870351a68861d
Crystal <crystal120599@gmail.com>
2023-11-15 10:59:28 -0800

    main task works

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+require('dotenv').config();
 
-let gifs = document.querySelector("#gifs");
+// Print out value of API key stored in .env file
+console.log(process.env.API_KEY)
+//let gifs = document.querySelector("#gifs");
 
 async function getImage(query){
-    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=dLXfl03PxqT8vv4VEBI8TFwLT713vLbD&q=${query}&limit=25&offset=0&rating=g&lang=en`
-    try{
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    //try{
         let result = await fetch(endpoint)
         let data = await result.json();
-        let arr = data.data.map(ele => ele.url);
-        for(let el of arr){
-            //console.log(el)
-            let img = document.createElement("img");
-            img.src = el;
-            gifs.appendChild(img);
-        }
+        //console.log(data.data[0].images)
+        let arr = data.data.map(ele => ele.images.original.url);
+        //console.log(arr)
+        // for(let el of arr){
+        //     //console.log(el)
+        //     let img = document.createElement("img");
+        //     img.src = el;
+        //     gifs.appendChild(img);
+        // }
+      // console.log(arr[0])
+       return arr[0]
+    // }catch(e){
+    //     console.log(e)
+    // }
 
-    }catch(e){
-        console.log(e)
-    }
 }
 
-//console.log(getImage("dogs"))
+console.log(getImage("dogs"))
 
-let button = document.querySelector("#button");
+// let button = document.querySelector("#button");
 
-button.addEventListener("click", (e) =>{
-    console.log("clicked")
-    let search = document.querySelector("input").value;
-    getImage(search)
-})
+// button.addEventListener("click", (e) =>{
+//     console.log("clicked")
+//     let search = document.querySelector("input").value;
+//     getImage(search)
+// })
